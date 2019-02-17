d3b5b978091139c2f35a64f42955cb8be21d156d
AmirMohammad <amirmhaghighat@gmail.com>
2019-02-17 12:16:42 +0330

    change footer redesign

--- footer.php
+++ footer.php
@@ -1,6 +1,295 @@
 <!-- Footer -->
 
+
+
 <footer class="footer">
+    <div class="container">
+        <div class="row">
+
+
+            <div class="col-lg-4 footer-left px-4">
+                <div class="logo-in-footer" ></div>
+                <p class="footer-company-about my-4">
+                اپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز  بهبود ابزارهای کاربردی می باشد
+                </p>
+
+                <div class="footer-phone-number d-flex align-items-center mb-3">
+                    <div hidden>
+                        <svg version="1.1" id="phone-keyboard" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            x="0px" y="0px" viewBox="0 0 522.753 522.752" style="enable-background:new 0 0 522.753 522.752;"
+                            xml:space="preserve">
+                            <g>
+                                <path d="M151.891,58.183c0-13.072-10.595-23.677-23.677-23.677h-80.87C21.2,34.506,0,53.937,0,77.901
+                                       c0,23.973,21.2,43.404,47.344,43.404h80.87c13.072,0,23.677-10.595,23.677-23.677V58.183z" />
+                                <path d="M337.317,58.183c0-13.072-10.596-23.677-23.677-23.677H209.104c-13.072,0-23.677,10.595-23.677,23.677v39.455
+                                       c0,13.072,10.595,23.677,23.677,23.677H313.65c13.071,0,23.677-10.595,23.677-23.677V58.183H337.317z" />
+                                <path d="M475.409,34.516h-80.87c-13.071,0-23.677,10.595-23.677,23.677v39.455c0,13.072,10.596,23.677,23.677,23.677h80.87
+                                       c26.145,0,47.344-19.431,47.344-43.404S501.553,34.516,475.409,34.516z" />
+                                <path d="M151.891,180.497c0-13.072-10.595-23.677-23.677-23.677h-80.87C21.2,156.82,0,176.251,0,200.215
+                                       c0,23.973,21.2,43.395,47.344,43.395h80.87c13.072,0,23.677-10.595,23.677-23.677V180.497z" />
+                                <path d="M337.317,180.497c0-13.072-10.596-23.677-23.677-23.677H209.104c-13.072,0-23.677,10.595-23.677,23.677v39.445
+                                       c0,13.072,10.595,23.677,23.677,23.677H313.65c13.071,0,23.677-10.596,23.677-23.677v-39.445H337.317z" />
+                                <path d="M475.409,156.83h-80.87c-13.071,0-23.677,10.595-23.677,23.677v39.445c0,13.072,10.596,23.677,23.677,23.677h80.87
+                                       c26.145,0,47.344-19.421,47.344-43.395S501.553,156.83,475.409,156.83z" />
+                                <path d="M151.891,302.801c0-13.072-10.595-23.677-23.677-23.677h-80.87C21.2,279.125,0,298.545,0,322.51
+                                       c0,23.973,21.2,43.404,47.344,43.404h80.87c13.072,0,23.677-10.596,23.677-23.678V302.801z" />
+                                <path d="M337.317,302.801c0-13.072-10.596-23.677-23.677-23.677H209.104c-13.072,0-23.677,10.595-23.677,23.677v39.445
+                                       c0,13.072,10.595,23.678,23.677,23.678H313.65c13.071,0,23.677-10.596,23.677-23.678v-39.445H337.317z" />
+                                <path d="M475.409,279.134h-80.87c-13.071,0-23.677,10.595-23.677,23.677v39.445c0,13.072,10.596,23.676,23.677,23.676h80.87
+                                       c26.145,0,47.344-19.43,47.344-43.404C522.753,298.555,501.553,279.134,475.409,279.134z" />
+                                <path d="M128.214,401.438h-80.87C21.2,401.438,0,420.87,0,444.833c0,23.975,21.2,43.404,47.344,43.404h80.87
+                                       c13.072,0,23.677-10.604,23.677-23.676v-39.455C151.891,412.034,141.286,401.438,128.214,401.438z" />
+                                <path d="M313.65,401.438H209.104c-13.072,0-23.677,10.596-23.677,23.678v39.455c0,13.062,10.595,23.676,23.677,23.676H313.65
+                                       c13.071,0,23.677-10.605,23.677-23.676v-39.455C337.317,412.034,326.722,401.438,313.65,401.438z" />
+                                <path d="M475.409,401.438h-80.87c-13.071,0-23.677,10.596-23.677,23.678v39.455c0,13.062,10.596,23.676,23.677,23.676h80.87
+                                       c26.145,0,47.344-19.432,47.344-43.404S501.553,401.438,475.409,401.438z" />
+                            </g>
+
+                        </svg>
+                    </div>
+                    <svg class="phone-keyboard">
+                        <use xlink:href="#phone-keyboard" />
+                    </svg>
+                    
+
+
+                    <div class="veresk-phone-number mx-2">
+                    ساعت ۸ تا ۹ -  ۹۸۲۱۶۶۱۷۵۳۲۲+
+                    </div>
+
+                </div>
+
+                <div class="support d-flex align-items-center mb-3">
+                    <div hidden>
+                        <svg version="1.1" id="at-sign" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            x="0px" y="0px" viewBox="0 0 490.2 490.2" style="enable-background:new 0 0 490.2 490.2;"
+                            xml:space="preserve">
+                            <g>
+                                <path d="M420.95,61.8C376.25,20.6,320.65,0,254.25,0c-69.8,0-129.3,23.4-178.4,70.3s-73.7,105.2-73.7,175
+		c0,66.9,23.4,124.4,70.1,172.6c46.9,48.2,109.9,72.3,189.2,72.3c47.8,0,94.7-9.8,140.7-29.5c15-6.4,22.3-23.6,16.2-38.7l0,0
+		c-6.3-15.6-24.1-22.8-39.6-16.2c-40,17.2-79.2,25.8-117.4,25.8c-60.8,0-107.9-18.5-141.3-55.6c-33.3-37-50-80.5-50-130.4
+		c0-54.2,17.9-99.4,53.6-135.7c35.6-36.2,79.5-54.4,131.5-54.4c47.9,0,88.4,14.9,121.4,44.7s49.5,67.3,49.5,112.5
+		c0,30.9-7.6,56.7-22.7,77.2c-15.1,20.6-30.8,30.8-47.1,30.8c-8.8,0-13.2-4.7-13.2-14.2c0-7.7,0.6-16.7,1.7-27.1l18.6-152.1h-64
+		l-4.1,14.9c-16.3-13.3-34.2-20-53.6-20c-30.8,0-57.2,12.3-79.1,36.8c-22,24.5-32.9,56.1-32.9,94.7c0,37.7,9.7,68.2,29.2,91.3
+		c19.5,23.2,42.9,34.7,70.3,34.7c24.5,0,45.4-10.3,62.8-30.8c13.1,19.7,32.4,29.5,57.9,29.5c37.5,0,69.9-16.3,97.2-49
+		c27.3-32.6,41-72,41-118.1C488.05,152.9,465.75,103,420.95,61.8z M273.55,291.9c-11.3,15.2-24.8,22.9-40.5,22.9
+		c-10.7,0-19.3-5.6-25.8-16.8c-6.6-11.2-9.9-25.1-9.9-41.8c0-20.6,4.6-37.2,13.8-49.8s20.6-19,34.2-19c11.8,0,22.3,4.7,31.5,14.2
+		s13.8,22.1,13.8,37.9C290.55,259.2,284.85,276.6,273.55,291.9z" />
+                            </g>
+
+                        </svg>
+
+                    </div>
+                    <svg class="at-sign">
+                        <use xlink:href="#at-sign" />
+                    </svg>
+
+                    <a href="mailto:info@veresk.org" target="_blank" class="support-email mx-3">
+                        info@veresk.org
+                    </a>
+
+                </div>
+
+            </div>
+
+
+            <div class="col-lg-2 footer-center py-5 px-4">
+
+                <div>
+                    <span class="header-in-footer-columns">
+                       صفحه اصلی
+                    </span>
+                </div>
+
+                <div hidden>
+                    <svg version="1.1" id="right-arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        x="0px" y="0px" viewBox="0 0 41.999 41.999" style="enable-background:new 0 0 41.999 41.999;"
+                        xml:space="preserve">
+                        <path d="M36.068,20.176l-29-20C6.761-0.035,6.363-0.057,6.035,0.114C5.706,0.287,5.5,0.627,5.5,0.999v40
+                       c0,0.372,0.206,0.713,0.535,0.886c0.146,0.076,0.306,0.114,0.465,0.114c0.199,0,0.397-0.06,0.568-0.177l29-20
+                       c0.271-0.187,0.432-0.494,0.432-0.823S36.338,20.363,36.068,20.176z" />
+                        <g>
+                        </g>
+                    </svg>
+
+                </div>
+
+                <ul class="p-0 mt-5">
+                    <a class="main-menu-item">
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                                خانه
+                            </span>
+
+                        </li>
+
+                    </a>
+                    <a class="main-menu-item">
+
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                                درباره ورسک
+                            </span>
+
+                        </li>
+                    </a>
+                    <a class="main-menu-item">
+
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                                خدمات
+                            </span>
+
+                        </li>
+                    </a>
+                    <a class="main-menu-item">
+
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                                ثبت نام کنید
+                            </span>
+                        </li>
+
+                    </a>
+                    <a class="main-menu-item">
+
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                                داشبورد
+                            </span>
+
+                        </li>
+                    </a>
+                </ul>
+
+            </div>
+
+            <div class="col-lg-2 footer-center py-5 px-4">
+
+                <div>
+                    <span class="header-in-footer-columns">
+                        داشبورد
+                    </span>
+                </div>
+
+                <div hidden>
+                    <svg version="1.1" id="right-arrow" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                        x="0px" y="0px" viewBox="0 0 41.999 41.999" style="enable-background:new 0 0 41.999 41.999;"
+                        xml:space="preserve">
+                        <path d="M36.068,20.176l-29-20C6.761-0.035,6.363-0.057,6.035,0.114C5.706,0.287,5.5,0.627,5.5,0.999v40
+                           c0,0.372,0.206,0.713,0.535,0.886c0.146,0.076,0.306,0.114,0.465,0.114c0.199,0,0.397-0.06,0.568-0.177l29-20
+                           c0.271-0.187,0.432-0.494,0.432-0.823S36.338,20.363,36.068,20.176z" />
+                        <g>
+                        </g>
+                    </svg>
+
+                </div>
+
+                <ul class="p-0 mt-5">
+                    <a class="main-menu-item">
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                               پرداخت ها
+                            </span>
+
+                        </li>
+
+                    </a>
+                    <a class="main-menu-item">
+
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                                ردیابی واگن
+                            </span>
+
+                        </li>
+                    </a>
+                    <a class="main-menu-item">
+
+                        <li>
+                            <svg class="right-arrow">
+                                <use xlink:href="#right-arrow" />
+                            </svg>
+                            <span class="px-2">
+                                خرید بلیط
+                            </span>
+
+                        </li>
+                    </a>
+
+                </ul>
+
+            </div>
+
+            <div class="col-lg-4 footer-right py-5 px-4">
+
+                <div>
+                    <span class="header-in-footer-columns">
+                       ثبت نام در خبرنامه هفته ای
+                    </span>
+                </div>
+
+                <p class="footer-company-about my-4">
+                آنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز  بهبود ابزارهای کاربردی می باشد
+                </p>
+
+                <div class="col-sm-12 mt-4 p-0">
+                    <label for="footer-email" class="input p-0">
+                        <input name="email" type="email" id="footer-email" placeholder="&nbsp;" required>
+                        <span class="label">ایمیل خود را وارد کنید</span>
+                        <span class="border"></span>
+                        <span class="send-icon"><i class="fa fa-envelope"></i></span>
+                    </label>
+                </div>
+                <div class="footer-icons">
+                    <a target="_blank" href="https://twitter.com/veresk_org"><i class="fa fa-twitter"></i></a>
+                    <a target="_blank" href="https://www.linkedin.com/company/veresk">
+                        <i class="fa fa-linkedin"></i>
+                    </a>
+                    <a target="_blank" href="https://www.instagram.com/veresk_org"><i class="fa fa-instagram"></i></a>
+                    <a target="_blank" href="https://t.me/veresk_org"><i class="fa fa-telegram"></i></a>
+                </div>
+
+            </div>
+            <hr class="mx-4">
+
+            <div class="d-flex justify-content-between footer-bottom col-sm-12 px-4">
+                <small>© 2019 Veresk Co.All Right Reserved</small>
+                <small>Veresk Train Tracking Dashboard</small>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+
+
+<!-- <footer class="footer">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 footer-left">
@@ -59,7 +348,7 @@
             </div>
         </div>
     </div>
-</footer>
+</footer> -->
 </div>
 
 </div>
