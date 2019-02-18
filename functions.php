c35e1582594c6aa3c84ea2a4752d45809ebf5f5b
AmirMohammad <amirmhaghighat@gmail.com>
2019-02-18 19:55:24 +0330

    change post content design in single page

--- functions.php
+++ functions.php
@@ -2,4 +2,4 @@
 add_theme_support( 'post-thumbnails' );
 
 // remove auto <p> before single page "the_content"
-remove_filter('the_content','wpautop');
+// remove_filter('the_content','wpautop');
--- single.php
+++ single.php
@@ -129,7 +129,120 @@ if (have_posts()) {
 
             <!-- Post Content -->
 
-            <div class="col-lg-9">
+            <!-- Share -->
+            <div class="col-lg-2 side_bar_single">
+                <div class="post_body d-flex flex-column align-items-center justify-content-between px-3">
+                    <div class="side_bar_categories d-flex flex-column align-items-center">
+                        <a class="social_medias d-flex justify-content-center align-items-center m-2">
+                            <div hidden>
+                                <svg version="1.1" id="instagram" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                    xml:space="preserve">
+                                    <g>
+                                        <g>
+                                            <path d="M352,0H160C71.648,0,0,71.648,0,160v192c0,88.352,71.648,160,160,160h192c88.352,0,160-71.648,160-160V160
+			C512,71.648,440.352,0,352,0z M464,352c0,61.76-50.24,112-112,112H160c-61.76,0-112-50.24-112-112V160C48,98.24,98.24,48,160,48
+			h192c61.76,0,112,50.24,112,112V352z" />
+                                        </g>
+                                    </g>
+                                    <g>
+                                        <g>
+                                            <path d="M256,128c-70.688,0-128,57.312-128,128s57.312,128,128,128s128-57.312,128-128S326.688,128,256,128z M256,336
+			c-44.096,0-80-35.904-80-80c0-44.128,35.904-80,80-80s80,35.872,80,80C336,300.096,300.096,336,256,336z" />
+                                        </g>
+                                    </g>
+                                    <g>
+                                        <g>
+                                            <circle cx="393.6" cy="118.4" r="17.056" />
+                                        </g>
+                                    </g>
+
+                                </svg>
+                            </div>
+                            <svg class="instagram">
+                                <use xlink:href="#instagram" />
+                            </svg>
+
+                        </a>
+
+                        <a target="_blank" href="https://t.me/share/url?url=<?php the_permalink();?>" class="social_medias d-flex justify-content-center align-items-center m-2">
+                            <div hidden>
+                                <svg version="1.1" id="telegram" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                    xml:space="preserve">
+                                    <path d="m640-1.667969-640 272.039063 167.777344 66.585937 59.726562 224.507813 109.976563-106.558594 178.917969 123.570312zm-403.78125 367.402344-6.457031 58.535156-24.800781-93.234375 435.039062-332.703125zm0 0" />
+                                </svg>
+                            </div>
+                            <svg class="telegram">
+                                <use xlink:href="#telegram" />
+                            </svg>
+                        </a>
+
+                        <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&summary=<?php the_title();?>&source=veresk.org"
+                            class="social_medias d-flex justify-content-center align-items-center m-2">
+                            <div hidden>
+                                <svg version="1.1" id="linkedin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                    xml:space="preserve">
+                                    <g>
+                                        <g>
+                                            <rect y="160" width="114.496" height="352" />
+                                        </g>
+                                    </g>
+                                    <g>
+                                        <g>
+                                            <path d="M426.368,164.128c-1.216-0.384-2.368-0.8-3.648-1.152c-1.536-0.352-3.072-0.64-4.64-0.896
+			c-6.08-1.216-12.736-2.08-20.544-2.08c-66.752,0-109.088,48.544-123.04,67.296V160H160v352h114.496V320
+			c0,0,86.528-120.512,123.04-32c0,79.008,0,224,0,224H512V274.464C512,221.28,475.552,176.96,426.368,164.128z" />
+                                        </g>
+                                    </g>
+                                    <g>
+                                        <g>
+                                            <circle cx="56" cy="56" r="56" />
+                                        </g>
+                                    </g>
+
+                                </svg>
+                            </div>
+                            <svg class="linkedin">
+                                <use xlink:href="#linkedin" />
+                            </svg>
+                        </a>
+
+                        <a target="_blank" href="http://twitter.com/share?text=<?php echo the_title(); ?>&url=<?php the_permalink();?>"
+                            class="social_medias d-flex justify-content-center align-items-center m-2">
+                            <div hidden>
+                                <svg version="1.1" id="twitter" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;"
+                                    xml:space="preserve">
+                                    <g>
+                                        <g>
+                                            <path d="M512,97.248c-19.04,8.352-39.328,13.888-60.48,16.576c21.76-12.992,38.368-33.408,46.176-58.016
+			c-20.288,12.096-42.688,20.64-66.56,25.408C411.872,60.704,384.416,48,354.464,48c-58.112,0-104.896,47.168-104.896,104.992
+			c0,8.32,0.704,16.32,2.432,23.936c-87.264-4.256-164.48-46.08-216.352-109.792c-9.056,15.712-14.368,33.696-14.368,53.056
+			c0,36.352,18.72,68.576,46.624,87.232c-16.864-0.32-33.408-5.216-47.424-12.928c0,0.32,0,0.736,0,1.152
+			c0,51.008,36.384,93.376,84.096,103.136c-8.544,2.336-17.856,3.456-27.52,3.456c-6.72,0-13.504-0.384-19.872-1.792
+			c13.6,41.568,52.192,72.128,98.08,73.12c-35.712,27.936-81.056,44.768-130.144,44.768c-8.608,0-16.864-0.384-25.12-1.44
+			C46.496,446.88,101.6,464,161.024,464c193.152,0,298.752-160,298.752-298.688c0-4.64-0.16-9.12-0.384-13.568
+			C480.224,136.96,497.728,118.496,512,97.248z" />
+                                        </g>
+                                    </g>
+
+                                </svg>
+                            </div>
+                            <svg class="twitter">
+                                <use xlink:href="#twitter" />
+                            </svg>
+                        </a>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-10">
 
                 <!-- Post Body -->
 
@@ -141,8 +254,8 @@ if (have_posts()) {
                         <?php the_author_posts_link()?>
                     </div>
 
-                    <!-- Post Tags and Share-->
-                    <div class="tags_share d-flex flex-column align-items-center">
+                    <!-- Post Tags -->
+                    <div class="tags_share d-flex flex-column">
                         <div class="post_tags">
                             <ul>
                                 <?php if (get_the_tags()[0]): ?>
@@ -156,82 +269,36 @@ if (have_posts()) {
                                 <?php endif?>
                             </ul>
                         </div>
-                        <div class="post_share my-3">
-                            <span>اشتراک گذاری:</span>
-                            <ul class="post_share_list">
-                                <a target="_blank" href="http://twitter.com/share?text=<?php echo the_title(); ?>&url=<?php the_permalink();?>">
-                                    <i class="fa fa-twitter"></i>
-                                </a>
-                                <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&summary=<?php the_title();?>&source=veresk.org"><i
-                                        class="fa fa-linkedin"></i></a>
-                                <!-- <a target="_blank" href="https://www.instagram.com/veresk_org"><i class="fa fa-instagram"></i></a> -->
-                                <a target="_blank" href="https://t.me/share/url?url=<?php the_permalink();?>"><i class="fa fa-telegram"></i></a>
-                            </ul>
-                        </div>
+
                     </div>
-                    <div class="track_your_wagon my-4 mt-5 p-5 row mx-0 align-items-center">
-                        <h4>
-                            مایل به ردیابی واگن یا بار خود هستید؟
-                        </h4>
-                        <a href="https://panel.veresk.org/dashboard/track-list?guest=true&lang=fa">
-                            <button>
-                                ردیابی کنید
-                            </button>
-                        </a>
-                    </div>
+                   
                 </div>
             </div>
 
-            <div class="col-lg-3 side_bar_single">
-                <div class="post_body d-flex flex-column align-items-center justify-content-between px-3">
-                    <div class="side_bar_categories d-flex flex-column align-items-center">
-                        دسته بندی ها:
-                        <div class="section_tags">
-                            <ul class="d-flex flex-column">
-                                <?php foreach (get_categories(array('orderby' => 'name', 'order' => 'ASC')) as $cat): ?>
-                                <li class="my-2 <?php if(is_category() && get_queried_object()->term_id == $cat->term_id) echo 'active'?>">
-                                    <a href="<?php echo get_category_link($cat->term_id); ?>">
-                                        <?php echo $cat->cat_name; ?>
-                                    </a>
-                                </li>
-                                <?php endforeach;?>
-                            </ul>
-                        </div>
+          
 
-                    </div>
-                    <div class="post_tags d-flex flex-wrap align-items-center justify-content-center p-4">
-                        <span class="mb-3">برچسب ها:</span>
-                        <ul>
-                            <?php if (get_the_tags()[0]): ?>
-                            <?php foreach (get_the_tags() as $tag): ?>
-                            <li class="post_tag">
-                                <a href="<?php echo get_tag_link($tag->term_id); ?>">
-                                    <span>
-                                        <?php echo $tag->name ?>
-                                    </span>
-                                </a>
-                            </li>
-                            <?php endforeach;?>
-                            <?php endif?>
-                        </ul>
-                    </div>
-                    <div class="social_media d-flex flex-column align-items-center py-3">
-                        ما را در شبکه های اجتماعی دنبال کنید:
-                        <ul class="my-4">
-                            <a target="_blank" href="http://twitter.com/share?text=<?php echo the_title(); ?>&url=<?php the_permalink();?>">
-                                <i class="fa fa-twitter fa-2x"></i>
-                            </a>
-                            <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink();?>&summary=<?php the_title();?>&source=veresk.org"><i
-                                    class="fa fa-linkedin fa-2x mx-1"></i></a>
-                            <a target="_blank" href="https://www.instagram.com/veresk_org"><i class="fa fa-instagram fa-2x mx-1"></i></a>
-                            <a target="_blank" href="https://t.me/share/url?url=<?php the_permalink();?>"><i class="fa fa-telegram fa-2x"></i></a>
-                        </ul>
-                    </div>
-                </div>
 
-            </div>
 
         </div>
     </div>
+<div class="comment_section">
+   <div class="container">
+                <div class="row d-flex justify-content-center py-5">
+                    <div class="col-sm-8">
+                        <div class="comment_body p-4">
+                            <div class="leave-comment">
+                                <div class="leave-comment-title col-sm-7">
+                                    نظر خود را بنویسید
+                                </div>
+
+                                <div class="leave-comment-text col-sm-4">
+                                برای ارسال نظر لطفا وارد شوید و یا ثبت نام کنید
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+   </div>
 </div>
 <?php get_footer();?>
