c4ce061e301ca021afca5e4debaa847898801756
AmirMohammad <amirmhaghighat@gmail.com>
2019-03-06 10:38:37 +0330

    change filter title and exerpt image size

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 <div class="container-fluid filter-and-search-container">
     <div class="container py-4 px-0">
         <div class="section_panel px-3 d-flex flex-row align-items-center justify-content-between flex-wrap">
-            <div class="filter_section d-flex align-items-center py-md-3"> <span>فیلتر:</span>
+            <div class="filter_section d-flex align-items-center py-md-3"> <span>دسته ها:</span>
                 <div class="section_tags px-3">
                     <ul>
                         <?php foreach (get_categories(array('orderby' => 'name', 'order' => 'ASC')) as $cat): ?>
@@ -29,7 +29,7 @@
             </div>
 
             <div class="search-in-tag-section">
-                <input class="form-control search-input" id="choices-text-preset-values" type="text" placeholder="جستجو را شروع کنید..." />
+                <input class="form-control search-input" id="choices-text-preset-values" type="text" placeholder="جستجو..." />
 
                 <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
