f42be275767181da58836ca7ccf51ee21ab53558
Majid Nayyeri <majidrobo@gmail.com>
2019-03-13 15:56:40 +0330

    fixes #15

--- index.php
+++ index.php
@@ -29,30 +29,37 @@
             </div>
 
             <div class="search-in-tag-section">
-                <input class="form-control search-input" id="choices-text-preset-values" type="text" placeholder="جستجو..." />
-
-                <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-                </svg>
-
+                <form method="get" id="search form" action="">
+                    <input class="form-control search-input" type="text" placeholder="جستجو..." name="s" id="s" />
+                    <button>
+                        <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                        </svg>
+                    </button>
+                </form>
             </div>
-
-
         </div>
     </div>
 </div>
 
 <div class="page_content row py-5 mx-0" id="page_content">
-
-
-
+    <?php if (is_search()): ?>
+    <div class="container d-flex flex-wrap my-4">
+        <div class="col-12 text-center">
+            <h3>
+                نتایج جستجو برای:
+                <b>
+                    <?php echo get_search_query(); ?>
+                </b>
+            </h3>
+        </div>
+    </div>
+    <?php endif; ?>
     <?php if (have_posts()): ?>
     <div class="container d-flex flex-wrap my-4">
         <?php while (have_posts()): the_post();?>
         <div class="col-md-6 col-lg-4">
-
             <div class="card">
-
                 <span class="circle-of-date">
                     <span class="date-of-post p-2">
                         <?php echo get_the_date('j F'); ?></span>
