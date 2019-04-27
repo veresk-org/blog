5d8dcc1b38a651745c6699b2c4c6236ea5685f95
Majid Nayyeri <majidrobo@gmail.com>
2019-04-27 17:26:59 +0430

    add image url to wp_json

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php
 add_theme_support( 'post-thumbnails' );
+function ws_register_images_field() {
+    register_rest_field( 
+        'post',
+        'images',
+        array(
+            'get_callback'    => 'ws_get_images_urls',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
 
-// remove auto <p> before single page "the_content"
-// remove_filter('the_content','wpautop');
+add_action( 'rest_api_init', 'ws_register_images_field' );
+
+function ws_get_images_urls( $object, $field_name, $request ) {
+    $medium = wp_get_attachment_image_src( get_post_thumbnail_id( $object->id ), 'medium' );
+    $medium_url = $medium['0'];
+
+    $large = wp_get_attachment_image_src( get_post_thumbnail_id( $object->id ), 'large' );
+    $large_url = $large['0'];
+
+    return array(
+        'medium' => $medium_url,
+        'large'  => $large_url,
+    );
+}
