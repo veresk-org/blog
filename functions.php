59ec5b8996cc6a61b021daa4cb492d8215f3dc6b
Majid Nayyeri <majidrobo@gmail.com>
2019-04-27 17:58:35 +0430

    add categories' names to wp-json

--- functions.php
+++ functions.php
@@ -1,28 +1,45 @@
 <?php
 add_theme_support( 'post-thumbnails' );
-function ws_register_images_field() {
+function register_images_field() {
     register_rest_field( 
         'post',
         'images',
         array(
-            'get_callback'    => 'ws_get_images_urls',
+            'get_callback'    => 'get_images_urls',
             'update_callback' => null,
             'schema'          => null,
         )
     );
 }
+function register_categories_names_field() {
+    register_rest_field( 
+        'post',
+        'categories',
+        array(
+            'get_callback'    => 'get_categories_names',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+add_action( 'rest_api_init', 'register_images_field' );
+add_action( 'rest_api_init', 'register_categories_names_field' );
 
-add_action( 'rest_api_init', 'ws_register_images_field' );
-
-function ws_get_images_urls( $object, $field_name, $request ) {
+function get_images_urls( $object, $field_name, $request ) {
     $medium = wp_get_attachment_image_src( get_post_thumbnail_id( $object->id ), 'medium' );
     $medium_url = $medium['0'];
-
     $large = wp_get_attachment_image_src( get_post_thumbnail_id( $object->id ), 'large' );
     $large_url = $large['0'];
-
     return array(
         'medium' => $medium_url,
         'large'  => $large_url,
     );
+}
+function get_categories_names( $object, $field_name, $request ) {
+    $formatted_categories = array();
+    $categories = get_the_category( $object['id'] );
+    foreach ($categories as $category) {
+        $formatted_categories[] = $category->name;
+    }
+    return $formatted_categories;
 }
