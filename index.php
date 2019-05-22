0755d2532d1c4af0563b34241e57692d539dacdf
AmirMohammad <amirmhaghighat@gmail.com>
2019-05-22 17:39:42 +0430

    change some styles in categories abd search section

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             <span class="col-lg-1 col-md-12 d-flex justify-content-center p-0">دسته ها:</span>
             <div
                 class="filter_section col-lg-7 col-md-12 d-flex justify-content-center justify-content-md-start align-items-center py-3">
-                <div class="section_tags px-3">
+                <div class="section_tags px-3 d-flex justify-content-center">
                     <ul class="d-none d-md-block">
                         <li class="my-2 <?php if(!is_category()) echo 'active'?>">
                             <a href="<?php echo get_home_url(); ?>">
@@ -49,19 +49,17 @@
                 </div>
             </div>
 
-            <div class="search-in-tag-section col-md-12 col-lg-4">
-                <form method="get" id="search form">
-                    <input class="form-control search-input" type="text" placeholder="جستجو..." name="s" id="s" />
-                    <button>
-                        <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24">
-                            <path
-                                d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
-                            </path>
-                        </svg>
-                    </button>
-                </form>
-            </div>
+            <form class="search-in-tag-section d-flex align-items-center col-md-12 col-lg-4" method="get" id="search form">
+                <input class="form-control search-input" type="text" placeholder="جستجو..." name="s" id="s" />
+                <button>
+                    <svg class="search-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24">
+                        <path
+                            d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z">
+                        </path>
+                    </svg>
+                </button>
+            </form>
         </div>
     </div>
 </div>
